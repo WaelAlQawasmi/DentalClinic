2556f15c61609d64b831170a9537c51bb8a107e9
waelalq <wael.abuomr@gmail.com>
2022-07-03 18:26:31 +0300

    mobile

---
+++ html.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body{overflow-x:hidden;
+    height:100%;
+    background-color: #a3b2b50a;}
+  .button {font-weight: bold;
+     border: none;
+    color: white;
+    padding: 15px 42px;
+    text-align: center;
+    text-decoration: none;
+    position: absolute;
+    top: 30%;
+  left: 41%;
+    font-size: 30px;;
+    margin: 200px 20px;
+    cursor: pointer;}
+  
+    .button2 {background-color: #229ab229;} /* Blue */
+    div.a {
+  text-align: center;
+}
+a{
+    text-decoration: none;
+    color:#1ec0c7;
+}
+
+
+
+p.ex1 {
+
+      text-align: center;
+    text-decoration: none;
+    display: block;
+    font-size: 50px;
+    cursor: pointer;
+    position: absolute;
+    top: 25%;
+    left: 25%;
+}
+
+
+</style>
+</head>
+<body>
+<p class="ex1">     لتحميل تطبيق عيادة الاسنان </p>
+<div class="a">
+<button class="button button2"> <a href="ebtesama.apk" download>اضغط هنا</a> </button>
+
+</div>
+</body>
+</html>
